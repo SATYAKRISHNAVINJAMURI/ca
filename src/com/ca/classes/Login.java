7c510b4627d27384cf83f44b42e9d289bc1ec269
Kaushik Dutta <kdutta@virtusa.com>
2019-08-13 20:12:36 +0530

    update

--- src/com/ca/classes/Login.java
+++ src/com/ca/classes/Login.java
@@ -2,16 +2,12 @@ package com.ca.classes;
 
 public class Login {
 	private int user_id;
-	private int faculty_id;
-	private int student_id;
 	private String password;
 	private String role;
 	
-	public Login(int user_id, int faculty_id, int student_id, String password, String role) {
+	public Login(int user_id, String password, String role) {
 		super();
 		this.user_id = user_id;
-		this.faculty_id = faculty_id;
-		this.student_id = student_id;
 		this.password = password;
 		this.role = role;
 	}
@@ -28,22 +24,7 @@ public class Login {
 		this.user_id = user_id;
 	}
 
-	public int getFaculty_id() {
-		return faculty_id;
-	}
-
-	public void setFaculty_id(int faculty_id) {
-		this.faculty_id = faculty_id;
-	}
-
-	public int getStudent_id() {
-		return student_id;
-	}
-
-	public void setStudent_id(int student_id) {
-		this.student_id = student_id;
-	}
-
+	
 	public String getPassword() {
 		return password;
 	}
--- src/com/ca/classes/StudentImplementationDao.java
+++ src/com/ca/classes/StudentImplementationDao.java
@@ -1,16 +1,44 @@
 package com.ca.classes;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import com.ca.interfaces.User;
+import com.ca.servlets.Ca;
 
 public class StudentImplementationDao implements User {
 	
-	public User getCredentials()
+		private String dbUrl="jdbc:mysql://10.7.177.21:3306/ca";
+		private String dbUser="causer";
+		private String dbPass="capass";
+		private String dbDriver="com.mysql.jdbc.Driver";
+	
+	private Connection connection;
+	User user;
+	try {
+		Class.forName(driverClass);
+		connection = DriverManager.getConnection(dbUrl,dbUser,dbPass);
+	}
+	catch (ClassNotFoundException | SQLException e) {
+		System.out.println(e);
+	}
+	
+	public User getCredentials(String user_id, String password, String role)
 	{
+		String template = "select * from login";
+		user = new StudentImplementationDao();
 		
-		
-		
+		try(PreparedStatement pstmt = connection.prepareStatement(template)){
+			pstmt.setString(1, book.getIsbn());
+			pstmt.setString(2, book.getTitle());
+			pstmt.setString(3, book.getAuthor());
+			pstmt.setString(4, book.getPrice());
+			pstmt.setString(5, book.getStock());
 		return null;
 	}
 	
+}
 }
 	
 
